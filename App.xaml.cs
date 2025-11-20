3034fe56572f4f64eb842bfb9b16338d000d2bfc
Віталій Дубський <Vitalii.Dubskyi@cs.khpi.edu.ua>
2025-11-20 13:50:45 +0200

    basic chat layout

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Uchat
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uchat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void minimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void maximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+                //restore logo
+                maximizeButton.Content = "\uE922";
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+                //maximize logo
+                maximizeButton.Content = "\uE923";
+            }
+        }
+
+        private void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
