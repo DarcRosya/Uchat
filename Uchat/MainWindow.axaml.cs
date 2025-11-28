2f7456ef08b43f12a11b155f82d692fa111c98d7
Baggerok <baggerf12@gmail.com>
2025-11-28 16:49:16 +0200

    implented existed code into server-frinedly relationship

--- Uchat/MainWindow.axaml.cs
+++ Uchat/MainWindow.axaml.cs
@@ -4,137 +4,14 @@ using Avalonia.Input;
 using Avalonia.Input.Platform;
 using Avalonia.Interactivity;
 using Avalonia.Media;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
 using System.Threading;
 
 namespace Uchat
 {
     public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private TextBlock textBlockChange = new TextBlock();
-        private string tempChatTextBox = "";
-
-        private void SendButton_Click(object? sender, RoutedEventArgs e)
-        {
-            int messagesCount = ChatMessagesPanel.Children.Count;
-            var message = chatTextBox.Text?.Trim();
-            if (!string.IsNullOrEmpty(message))
-            {
-                replyTheMessageBox.IsVisible = false;
-
-                var textBlock = new TextBlock
-                {
-                    Text = message,
-                    HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Left,
-                    FontSize = 16,
-                    Background = Brushes.Transparent,
-                    Foreground = Brushes.White,
-                    MaxWidth = 500,
-                    TextWrapping = TextWrapping.Wrap
-                };
-
-                var bubble = new Border
-                {
-                    CornerRadius = new CornerRadius(5),
-                    Padding = new Thickness(8),
-                    Margin = new Thickness(5, 0, 5, 0),
-                    Child = textBlock,
-                    Background = Brush.Parse("#358c8f"),
-                    HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Right
-                };
-
-                var grid = new Grid
-                {
-                    ColumnDefinitions =
-                    {
-                        new ColumnDefinition(new GridLength(1, GridUnitType.Star))
-                    }
-                };
-
-                Grid.SetColumn(bubble, 0);
-                grid.Children.Add(bubble);
-                //Recieve message
-                if (messagesCount % 2 == 1)
-                {
-                    bubble.Background = Brush.Parse("#264c6f");
-                    bubble.HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Left;
-                }
-
-                ChatMessagesPanel.Children.Add(grid);
-                chatTextBox.Text = string.Empty;
-                ChatScrollViewer.ScrollToEnd();
-
-                var contextMenu = new ContextMenu();
-                bubble.ContextMenu = contextMenu;
-
-                MenuItem menuItemReply = new MenuItem
-                {
-                    Header = "Reply",
-                };
-
-                menuItemReply.Click += (s, e) =>
-                {
-                    chatTextBoxForReplyAndEdit.IsVisible = false;
-                    chatTextBox.IsVisible = true;
-                    editTheMessageBox.IsVisible = false;
-                    editTheMessageButton.IsVisible = false;
-                    replyTheMessageBox.IsVisible = true;
-                    replyTheMessage.Text = textBlock.Text;
-                };
-                contextMenu.Items.Add(menuItemReply);
-
-                MenuItem menuItemEdit = new MenuItem
-                {
-                    Header = "Edit"
-                };
-
-                menuItemEdit.Click += (s, e) =>
-                {
-                    tempChatTextBox = chatTextBox.Text;
-
-                    replyTheMessageBox.IsVisible = false;
-                    chatTextBox.IsVisible = false;
-
-                    editTheMessageBox.IsVisible = true;
-                    chatTextBoxForReplyAndEdit.IsVisible = true;
-                    editTheMessageButton.IsVisible = true;
-                    editTheMessage.Text = textBlock.Text;
-                    chatTextBoxForReplyAndEdit.Text = textBlock.Text;
-
-                    textBlockChange = textBlock;
-                };
-                contextMenu.Items.Add(menuItemEdit);
-
-                MenuItem menuItemCopy = new MenuItem
-                {
-                    Header = "Copy"
-                };
-
-                menuItemCopy.Click += (s, e) =>
-                {
-                    Clipboard.SetTextAsync(textBlock.Text);
-                };
-                contextMenu.Items.Add(menuItemCopy);
-
-                MenuItem menuItemDelete = new MenuItem
-                {
-                    Header = "Delete",
-                };
-
-                menuItemDelete.Click += (s, e) =>
-                {
-                    editTheMessageBox.IsVisible = false;
-                    replyTheMessageBox.IsVisible = false;
-                    ChatMessagesPanel.Children.Remove(grid);
-                };
-                contextMenu.Items.Add(menuItemDelete);
-            }
-        }
-
+    {   
         private void DontReplyTheMessage_Click(object? sender, RoutedEventArgs e)
         {
             replyTheMessageBox.IsVisible = false;
