6dedfb107745eeaf230fc5ce7b249be75dad227b
Vitalii Dubskyi <Vitalii.Dubskyi@cs.khpi.edu.ua>
2025-12-01 13:39:51 +0200

    idk what i did

--- Uchat/Assets/Code/ChatMessage.cs
+++ Uchat/Assets/Code/ChatMessage.cs
@@ -1,13 +1,7 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Data;
 using Avalonia.Interactivity;
-using Avalonia.Layout;
 using Avalonia.Media;
-using Avalonia.Media.Imaging;
-using Avalonia.Platform;
-using System;
-using System.Runtime.InteropServices.Marshalling;
+
 
 namespace Uchat
 {
